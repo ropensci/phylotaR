8b8cd644301231a73fecf87e30e9db53a496a5b6
hettling <hannes.hettling@gmail.com>
2016-07-28 17:59:55 +0200

    added functions to get children, remote and local

--- R/ncbi-tax.R
+++ R/ncbi-tax.R
@@ -1,6 +1,3 @@
-require('CHNOSZ')
-require('rentrez')
-require('data.table')
 
 ncbi.taxonomic.ranks <<- c('superkingdom', 'kingdom', 'subkingdom', 'superphylum', 'phylum',
 					   'subphylum', 'superclass', 'class', 'subclass', 'infraclass', 'superorder',
@@ -42,7 +39,7 @@ gis.all.for.taxid <- function( taxid, dir='.' ) {
 acc.genbank.seqids.for.taxid <- function( taxids, local=FALSE, dir=NULL ) {    
     if ( local ) {
         if ( is.null(dir) ) {
-            stop( "Need 'dir' argument if 'local=TRUE'" )
+            stop( "Need 'dir' argument if 'local==TRUE'" )
         }
         else {
             return( .local.acc.genbank.seqids.for.taxid( taxids, dir ) )
@@ -110,23 +107,71 @@ acc.genbank.seqids.for.taxid <- function( taxids, local=FALSE, dir=NULL ) {
     return(ret)    
 }
 
-get.all.child.taxa <- function(taxa, rank='species') {
-    db <- 'ncbi'
 
-    queue <- taxa
-    result <- data.frame(ncbi.id=integer(0), name=character(0), rank=character(0))
-    while ( length(queue) > 0 ) {
-        taxon <- queue[1]
-        ##cat("Taxon : ", taxon, "\n")
+get.children <- function(taxa, depth=-1, dir=NULL, local=FALSE) {
+    if (local && is.null(dir)) {
+        stop( "Need 'dir' argument if 'local==TRUE'" )
+    }
+    ret <- do.call(rbind, lapply(taxa, .get.children, depth, dir))
+    if (nrow(ret)) {
+        rownames(ret) <- 1:nrow(ret)
+    }
+    return(ret)
+}
+
+.get.children <- function(taxon, depth, dir) {    
+    queue <- c(taxon)
+    result <- data.frame(id=integer(0), rank=character(0))
+    while ( length(queue) > 0  & depth > 0) {
+        depth <- depth -1
+        curr.taxon <- queue[1]
         
         ## remove first element from queue
-        queue <- tail(queue, length(queue)-1)        
-
-        ch <- children(taxon, db=db)
-        table <- ch[[1]]       
-        ##        result <- rbind(result, subset(table, childtaxa_rank==rank))
-        result <- rbind(result, table)
-        queue <- c(queue, table$childtaxa_name)        
+        queue <- tail(queue, length(queue)-1)                
+        
+        ## get children
+        ch <- data.frame()
+        if (is.null(dir)) {
+            ch <- .remote.children(curr.taxon)
+        } else {
+            ch <- .local.children(curr.taxon, dir)
+        }
+        if (nrow(ch)){
+            result <- rbind(result, ch)
+            ## id 1 is it's own parent, therefore have to remove from queue
+            queue <- c(queue, ch[which(ch[,'id']!=1), 'id'])        
+        }
     }
     return (result)
+}
+
+.remote.children <- function(tax) {
+    ret <- data.frame(id=numeric(), rank=character())
+    db <- 'ncbi'   
+    ch <- taxize::children(tax, db)
+    ## Check for unsuccesful query
+    if (! is.na (ch)) {
+        df <- ch[[1]]
+        if (nrow(df)) {
+            ret <- data.frame(id=df[,'childtaxa_id'], rank=df[,'childtaxa_rank'])
+        }
+    }
+    return(ret)
+}
+
+.local.children <- function(tax, dir) {
+    ## buffer nodes object if possible
+    if (! exists('nodes')) {
+        nodes <<- CHNOSZ::getnodes(dir)
+    }
+    ch <- nodes[which(nodes['parent']==tax),c('id', 'rank')]
+    return(ch[,c('id', 'rank')])
+}
+
+
+    
+get.managable.nodes <- function(root.taxon, max.seqs=20000) {
+
+    
+    
 }
