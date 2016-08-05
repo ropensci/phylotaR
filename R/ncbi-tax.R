503d71940f55e7c6495cd6413c7bcb6143935c42
hettling <hannes.hettling@gmail.com>
2016-08-05 15:16:47 +0200

    add script to make table of nodes to cluster

--- R/ncbi-tax.R
+++ R/ncbi-tax.R
@@ -14,35 +14,48 @@ if (!exists('ncbi.names'))
 ##.import nucl_gb.accession2taxid accession2taxid
 ##create index taxid on accession2taxid(taxid);
 
-con <<- dbConnect(RSQLite::SQLite(),'~/ncbi-taxonomy/ftp.ncbi.nih.gov/pub/taxonomy/accession2taxid/mytest')
+con <<- dbConnect(RSQLite::SQLite(),'~/ftp.ncbi.nih.gov/pub/taxonomy/accession2taxid/mytest')
 
 get.cluster.nodes <- function(root.taxon, max.seqs=20000) {
     ## get descendants of root.taxon which have < max.seq sequences
     ## if a node has > max.seqs sequences, further traverse down the tree
-    result <- as.data.frame(matrix(ncol=4, nrow=0))
+    result <- as.data.frame(matrix(ncol=5, nrow=0))
     queue <- c(root.taxon)    
     while( length(queue) > 0 ) {
         cat("Queue : ", paste(queue, sep=','), "\n")
         curr.taxon <- queue[1]        
         cat("Current taxon : ", curr.taxon, "\n")
         queue <- tail(queue, length(queue)-1)                      
+
         curr.nof.seqs <- num.seqs.for.taxid(curr.taxon)
         cat("Number of seqs : ", curr.nof.seqs, "\n")        
         ch <- children(curr.taxon)
         rank <- getrank(curr.taxon, taxdir, ncbi.nodes)
-        if (curr.nof.seqs > max.seqs && length(ch) > 0  && ! rank %in% c('species', 'subspecies','varietas', 'forma')) {
-            cat("Number of seqs larger than max.seqs, getting children\n")
 
-            ##cat("Number of children : ", length(ch), "\n")
-            queue <- c(queue, ch)
+        if (curr.nof.seqs < max.seqs || length(ch) == 0 || rank %in% c('species', 'subspecies','varietas', 'forma')) {
+            name <- sciname(curr.taxon, taxdir, ncbi.names)
+            cat("Found node for taxon ", name, "\n")            
+            cluster.type <- ifelse(length(ch) > 0, 'subtree', 'node')
+            ## append to result table
+            result[nrow(result)+1,] <- c(curr.taxon, name, rank, curr.nof.seqs, cluster.type) 
+            ## In some cases, species can have a large amount of sequences but also have sub-species
+            ## with very few sequences (e.g. species Homo sapiens). Opposed to higher-level
+            ## taxa, the sum of a species' descendants number of sequences is not equal to the number
+            ## of sequences for that species. For species with a huge amount of seqs, we should therefore
+            ## give the subspecies (or varietas, forma etc)
+            ## the chance to have their own cluster!
+            if ( curr.nof.seqs > max.seqs && rank %in% c('species', 'subspecies','varietas', 'forma') ) {
+                queue <- c(queue, ch)
+            }
         }
         else {
-            name <- sciname(curr.taxon, taxdir, ncbi.names)
-            cat("Found manageable node for taxon ", name, "\n")
-            result[nrow(result)+1,] <- c(curr.taxon, name, rank, curr.nof.seqs)
-        }        
+            cat("Number of seqs larger than max.seqs, adding children to queue\n")
+            cat("Number of children : ", length(ch), "\n")
+            queue <- c(queue, ch)
+            
+        }
     }
-    colnames(result) <- c("taxid", "name", "rank", "seqs")
+    colnames(result) <- c("taxid", "name", "rank", "seqs", "type")
     return (result)        
 }
 
@@ -57,8 +70,8 @@ num.seqs.for.taxid <- function(taxid) {
 
 gis.for.taxid <- function(taxid) {
     ## taxid could be higher level, therefore include all descendants
-    ch <- c(id, descendants(taxid))
-    idstr <- paste0(ch, collapse=',')
+    ids <- c(taxid, descendants(taxid))
+    idstr <- paste0(ids, collapse=',')
     str <- paste('select gi from accession2taxid where taxid in (', idstr, ')')
     l <- dbGetQuery(con, str)
     return(l[[1]])        
