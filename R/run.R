304e7eed844330aa910fb9b7e9099b7bce6b00cc
hettling <hannes.hettling@gmail.com>
2017-10-08 20:47:06 +0200

    making felidae example, documenting

--- R/run.R
+++ R/run.R
@@ -1,5 +1,4 @@
 ## script to make the phylota tables
-
 source('nodes.R')
 source('clusters.R')
 source('ci_gi.R')
@@ -7,47 +6,32 @@ source('cl.R')
 library(foreach)
 library(doMC)
 
+set.seed(111)
+
 options(error=recover)
 
-## parse command args for root taxon
-#args <- commandArgs(trailingOnly=TRUE)
-##root.taxon <- args[1]
-
-dbname <- 'phylota.sqlite'
-taxdir <- '/home/hettling/taxdump'##'/Users/hettling/ftp.ncbi.nih.gov/pub/taxonomy'
-
-MODEL.THRESHOLD <<- 3000 ## 10000
-MAX.BLAST.SEQS <<- 10000 ## 100000
+## Adjustable parameters:
+## Maximum number of sequences per species
+MODEL.THRESHOLD <<- 3000
+## Maximum number of sequences to blast in a single run; if taxon has more subtree sequences
+## than that, its children will get clustered
+MAX.BLAST.SEQS <<- 10000
+## Maximum characters in one sequence
 MAX.SEQUENCE.LENGTH <<- 25000
+## directory for sequence cache; will be created if does not exist
 SEQS.CACHE.DIR <<- "./sequences/"
+## Download file ftp://ftp.ncbi.nlm.nih.gov/pub/taxonomy/taxdump.tar.gz,
+## unzip and specify directory where file 'nodes.dmp' is located
+taxdir <- '/Users/hettling/taxdump'
+## Number of processing units
 CORES <<- 4
 
 registerDoMC(CORES)
 
-##ncbi.names <<- getnames(taxdir)
+## Do analysis for Felidae family
+taxid <- 9681
+nodes.create(taxid, taxdir=taxdir, file.name='dbfiles-felidae-nodes.tsv')
 
-## root <- "Mammalia"
-##root <- "Felidae"
-##root.taxa <- ncbi.names$id[which(ncbi.names$name == root)]
-##cat("Taxid(s) to analyse : ", root.taxa)
-
-##remote.nodes.create(root.taxa=root.taxa, file.name='nodes-panthera.tsv')
-#root.taxa <- 9681
-#nodes.create(taxdir, root.taxa=root.taxa, file.name='nodes-felidae.tsv', model.threshold=10000)
-
-clusters.ci_gi.seqs.create(338152, 'nodes-felidae.tsv', files=list(clusters='dbfiles-felinae-clusters.tsv',
-                                                          ci_gi='dbfiles-felinae-ci_gi.tsv',
-                                                          seqs='dbfiles-felinae-seqs.tsv'))
-
-
-
-## cl <- cluster(9681, nodes)
-##cl <- cluster(37028, nodes)
-
-
-
-##.db(dbname)
-##cl <- clusters.ci_gi.seqs.create(root.taxa, 'nodes-felinae.tsv',
-##                                 files=list(clusters='dbfiles-felinae-clusters.tsv',
-##                                     ci_gi='dbfiles-felinae-ci_gi.tsv',
-##                                    seqs='dbfiles-felinae-seqs.tsv'), max.seqs=10000)
+clusters.ci_gi.seqs.create(338152, 'dbfiles-felidae-nodes.tsv', files=list(clusters='dbfiles-felidae-clusters.tsv',
+                                                            ci_gi='dbfiles-felidae-ci_gi.tsv',
+                                                            seqs='dbfiles-felidae-seqs.tsv'))
