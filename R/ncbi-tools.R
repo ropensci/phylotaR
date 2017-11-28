332c30d421f37dc1c76b669bcf2301a5a5a8e6b9
Dom Bennett <dominic.john.bennett@gmail.com>
2017-11-28 15:14:21 +0100

    New NCBI tools tested

--- R/ncbi-tools.R
+++ R/ncbi-tools.R
@@ -52,3 +52,118 @@ nSqs <- function(txid, direct=FALSE,
                   verbose=verbose)
   res[['count']]
 }
+
+#' @title dwnldSqs
+#' @description Given a taxon ID, queries NCBI
+#' for sequences that match the taxid.
+#' @param txid NCBI taxon identifier
+#' @param direct Whether to download all 'directly' linked
+#' sequences ([Organism:noexp] in Genbank search) or all 'subtree'
+#' sequences ([Organism:exp])
+#' @param mx_lngth Maximum length of sequence to be included in GI list
+#' @param mx_sqs Take a random subset of mx_sqs if the number of
+#' seqs exceeds it.
+#' Only makes sense when direct=TRUE
+#' @return list of lists containing sequence objects
+#' @export
+dwnldSqs <- function(txid, direct=FALSE,
+                     mx_lngth=25000,
+                     mx_sqs=100000,
+                     verbose=FALSE) {
+  # test w/ golden moles 9389
+  org_trm <- ifelse(direct, '[Organism:noexp]',
+                     '[Organism:exp]')
+  allseqs <- numeric()
+  args <- list(db='nucleotide',
+               term=paste0('txid', txid, org_trm, '1:',
+                           mx_lngth, '[SLEN]'),
+               use_history=TRUE,
+               ## XXX Without 1e9-1 one could save time...
+               # but are the hits random???
+               # @Hannes: from exp. they're not random,
+               #  easily tested though.
+               retmax=1e9-1)
+  srch <- safeSrch(func=rentrez::entrez_search,
+                   args=args, fnm='search',
+                   verbose=FALSE)
+  gis <- srch[['ids']];
+  if(length(gis) < 1) {
+    return(list())
+  }
+  # If the maximum amount of number is exceeded,
+  # randomly pick mx_sqs gis
+  if(direct && length(gis) > mx_sqs) {
+    .cp(v=verbose, "Choosing [", mx_sqs,
+        "] random sequences from [",
+        length(gis), "] available")
+    gis <- gis[sample(1:mx_sqs, replace=FALSE)]
+  }
+  .cp(v=verbose, "Retrieving [", length(gis),
+      "] sequences for taxid [", txid, "]")
+  # Fetch sequences in batches
+  btch <- 500
+  for(i in seq(0, length(gis)-1, btch)) {
+    ## Get FASTA strings for IDs in the specified segment
+    .cp(v=verbose, "Retreiving seqs [",
+        i+1, "-", ifelse(i+btch<length(gis),
+               i+btch, length(gis)),
+        "] for taxid [", txid, "]");
+    seqargs <- list(db="nuccore",
+                    rettype="fasta",
+                    web_history=srch[['web_history']],
+                    retmax=btch,
+                    retstart=i)
+    res <- safeSrch(func=rentrez::entrez_fetch,
+                    fnm='fetch', args=seqargs,
+                    verbose=verbose)
+    # split fasta string on '>'
+    seqstrs <- unlist(strsplit(res, "(?<=[^>])(?=\n>)",
+                               perl=TRUE))
+    # clear defline
+    seqstrs <- gsub("^\n?>.*?\\n", "", seqstrs)
+    # clear newlines
+    seqstrs <- gsub("\n", "", seqstrs, fixed=TRUE)
+    # Get summary object to retreive taxid, accession,
+    # and other parameters for seqs
+    summaries <- safeSrch(func=rentrez::entrez_summary,
+                          fnm='summary',
+                          args=list(db='nucleotide',
+                                    web_history=srch[['web_history']],
+                                    retmax=btch,
+                                    retstart=i),
+                          verbose=verbose)
+    # cnvrt to list when only one result
+    if(length(seqstrs)==1) {
+      summaries <- list(summaries)
+    }
+    seqs <- lapply(1:length(seqstrs), function(i) {
+      sm <- summaries[[i]]
+      se <- seqstrs[[i]]
+      seq <- list(gi=sm$uid,
+                  ti=sm$txid,
+                  acc=sm$caption,
+                  acc_vers=sm$accessionversion,
+                  length=sm$slen,
+                  ##TODO: Division not in summary object, how to get?
+                  division=NA,
+                  acc_date=sm$createdate,
+                  ##TODO: GBrel not in summary object, how to get?
+                  gbrel=NA,
+                  def=sm$title,
+                  seq=se)
+      seq
+    })
+    .cp(v=verbose, "Done retreiving [", length(seqs),
+        "(", i+1, "-",
+        ifelse(i+btch<length(gis),
+               i+btch, length(gis)),
+        ")] seqs for taxid [", txid, "]")
+    
+    allseqs <- c(allseqs, seqs)
+  }
+  .cp(v=verbose, "Finished retreiving [",
+      length(allseqs), "] sequences for taxid [",
+      txid, "]")
+  names(allseqs) <- gis
+  allseqs
+}
--- tests/testthat/test-ncbi-tools.R
+++ tests/testthat/test-ncbi-tools.R
@@ -2,6 +2,52 @@
 library(phylotaR)
 library(testthat)
 
+# FUNCTIONS
+randFasta <- function() {
+  seq <- sample(c('A', 'T', 'C', 'G'), size=1000,
+                replace=TRUE)
+  seq <- paste(seq, collapse='')
+  paste0('> rand0m.name.--jg82-5969\n', seq, '\n')
+}
+randSummary <- function() {
+  list(uid=NA, txid=NA, caption=NA,
+       accessionversion=NA,
+       slen=NA, createdate=NA, title=NA)
+}
+# stubs
+mckEntrezSearch <- function(db, term,
+                            use_history,
+                            retmax) {
+  if(n == 0) {
+    ids <- NULL
+  } else {
+    ids <- as.character(1:n)
+  }
+  list('ids'=ids)
+}
+mckEntrezFetch <- function(db, rettype,
+                           web_history,
+                           retmax, retstart) {
+  mx <- ifelse(retmax<n, retmax, n)
+  seqs <- ''
+  for(i in (retstart+1):(mx+retstart)) {
+    seqs <- paste0(seqs, randFasta())
+  }
+  seqs
+}
+mckEntrezSummary <- function(db, web_history,
+                             retmax, retstart) {
+  if(n == 1) {
+    return(randSummary())
+  }
+  mx <- ifelse(retmax<n, retmax, n)
+  summ <- vector('list', length=n)
+  for(i in (retstart+1):(mx+retstart)) {
+    summ[[i]] <- randSummary()
+  }
+  summ
+}
+
 # RUNNING
 context('Testing \'ncbi-tools\'')
 test_that('safeSrch() works', {
@@ -50,4 +96,44 @@ test_that('nSqs', {
     nSqs(txid=9606, direct=TRUE)
   )
   expect_true(grepl(':noexp', res[['term']]))
+})
+test_that('dwnldSqs', {
+  # n determines the number of available seqs.
+  n <- 0
+  res <- with_mock(
+    `rentrez::entrez_search`=mckEntrezSearch,
+    `rentrez::entrez_fetch`=mckEntrezFetch,
+    `rentrez::entrez_summary`=mckEntrezSummary,
+    dwnldSqs(txid=1, direct=FALSE, mx_lngth=25000,
+             mx_sqs=100000, verbose=FALSE)
+  )
+  expect_true(class(res) == 'list')
+  expect_true(length(res) == 0)
+  n <<- 1
+  res <- with_mock(
+    `rentrez::entrez_search`=mckEntrezSearch,
+    `rentrez::entrez_fetch`=mckEntrezFetch,
+    `rentrez::entrez_summary`=mckEntrezSummary,
+    dwnldSqs(txid=1, direct=FALSE, mx_lngth=25000,
+             mx_sqs=100000, verbose=FALSE)
+  )
+  expect_true(length(res) == 1)
+  n <<- 100
+  res <- with_mock(
+    `rentrez::entrez_search`=mckEntrezSearch,
+    `rentrez::entrez_fetch`=mckEntrezFetch,
+    `rentrez::entrez_summary`=mckEntrezSummary,
+    dwnldSqs(txid=1, direct=FALSE, mx_lngth=25000,
+             mx_sqs=100000, verbose=FALSE)
+  )
+  expect_true(length(res) == 100)
+  n <<- 1000
+  res <- with_mock(
+    `rentrez::entrez_search`=mckEntrezSearch,
+    `rentrez::entrez_fetch`=mckEntrezFetch,
+    `rentrez::entrez_summary`=mckEntrezSummary,
+    dwnldSqs(txid=1, direct=FALSE, mx_lngth=25000,
+             mx_sqs=100000, verbose=FALSE)
+  )
+  expect_true(length(res) == 1000)
 })
