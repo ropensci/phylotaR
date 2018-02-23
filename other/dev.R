96616a53d81476c5810e3bd220b4c8858dda761d
Dom Bennett <dominic.john.bennett@gmail.com>
2018-02-23 16:15:15 +0100

    Developing feature extraction

--- other/dev.R
+++ other/dev.R
@@ -1,40 +1,40 @@
-library(ips)
-load(file='dev_data.RData')
-wd <- '/home/dom/Desktop/testing_phylotaR/aotus'
-phylt_nds <- ldObj(wd=wd, nm='phylt_nds')
+# VARS
+id <- 'AJ428944'
+# AJ428944, AF434826
+mn_lngth <- 250
+mx_lngth <- 2500
 
-names(clstrs[[1]])
+# RETRIEVE
+raw_rcrd <- rentrez::entrez_fetch(db="nucleotide",
+                                 rettype='gbwithparts',
+                                 retmode='text', id=id)
+rcrd <- XML::xmlToList(raw_rcrd)
 
-clstrs[[1]]
-pull <- sapply(clstrs, function(x) length(unique(x[['tis']]))) > 10
-pull <- (sapply(clstrs, function(x) x[['MinLength']]) > 500) & pull
-sapply(clstrs[pull], function(x) length(x[['tis']]))
-clstrs[[]]
+# KEY INFO
+id <- rcrd[['GBSeq']][['GBSeq_primary-accession']]
+ml_typ <- rcrd[['GBSeq']][['GBSeq_moltype']]
 
-which(pull)[1]
-
-clstr_sqs <- sqs[clstrs[[20]][['gis']]]
-
-sq <- clstr_sqs[[1]]
-fastasqs <- rep(NA, length(clstr_sqs))
-for(i in 1:length(clstr_sqs)) {
-  sq <- clstr_sqs[[i]]
-  fastasq <- paste0('>', sq[['gi']], '|',
-                    sq[['ti']], '\n',
-                    sq[['seq']], '\n\n')
-  fastasqs[[i]] <- fastasq
+# FEATURE
+fltr_tbl <- rcrd[['GBSeq']][['GBSeq_feature-table']]
+fltr <- fltr_tbl[[2]]
+ids <- nms <- strts <- stps <- NULL
+for(i in seq_along(fltr_tbl)) {
+  fltr <- fltr_tbl[[i]]
+  print(i)
+  if(fltr[['GBFeature_key']] == 'gene') {
+    lctn <- fltr[['GBFeature_location']]
+    if(!grepl('^[0-9\\.]+$', lctn)) {
+      next
+    }
+    strtstp <- strsplit(x=lctn, split='\\.\\.')[[1]]
+    strtstp <- as.numeric(strtstp)
+    sqlngth <- strtstp[2] - strtstp[1]
+    if(sqlngth > mn_lngth & sqlngth < mx_lngth) {
+      nm <- fltr[['GBFeature_quals']][['GBQualifier']][['GBQualifier_value']]
+      nms <- c(nms, nm)
+      strts <- c(strts, strtstp[1])
+      stps <- c(stps, strtstp[2])
+      ids <- c(ids, paste0(id, '/', lctn))
+    }
+  }
 }
-x <- read.fas(text=fastasqs)
-align <- mafft(x=x, path='/usr/bin/mafft')
-tree <- raxml(DNAbin=align, m="GTRGAMMA",
-              f="a", N=10, p=1234, x=1234, 
-              exec='/usr/bin/raxmlHPC')
-tree[[1]]
-is(tree)
-plot(tree[[2]])
-
-phylo <- tree[[2]]
-library(treeman)
-tree <- as(phylo, 'TreeMan')
-
-calcDstMtrx(tree, tree['tips'])
