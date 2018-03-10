bdf617cb625c673a176be65ff6a4b53ca9a8207b
Dom Bennett <dominic.john.bennett@gmail.com>
2018-03-10 17:51:33 +0100

    Updated vignette files

--- vignettes/phylotaR.R
+++ vignettes/phylotaR.R
@@ -3,7 +3,7 @@
 #  wd <- '[YOUR PATH TO AOTUS FOLDER]'
 #  ncbi_dr <- '[YOUR PATH TO NCBI BLAST TOOLS]'
 #  txid <- 9504
-#  setUp(wd=wd, txid=txid, ncbi_dr=ncbi_dr)
+#  setUp(wd=wd, txid=txid, ncbi_dr=ncbi_dr, v=TRUE)
 
 ## ----running, eval=FALSE, message=FALSE, warning=FALSE, include=TRUE, paged.print=FALSE----
 #  run(wd=wd)
@@ -12,15 +12,47 @@
 #  reset(wd=wd, stage='cluster')
 #  restart(wd=wd)
 
-## ----selection, eval=FALSE, message=FALSE, warning=FALSE, include=TRUE, paged.print=FALSE----
-#  clstrs_obj <- genClstrsObj(wd)
-#  clstr_id <- getBestClstrs(clstrs_obj, n=1)
-#  writeSeqs(clstrs_obj, clstr_id, wd) # writes sequences to [clstr_id] + .fasta
+## ----selection1, eval=FALSE, message=FALSE, warning=FALSE, include=TRUE, paged.print=FALSE----
+#  all_clustrs <- read_phylota(wd)
+#  cids <- all_clustrs@cids
+#  n_taxa <- get_ntaxa(phylota=all_clustrs, cid=cids)
+
+## ----selection2, eval=FALSE, message=FALSE, warning=FALSE, include=TRUE, paged.print=FALSE----
+#  keep <- cids[n_taxa > 6]
+#  selected <- drop_cls(phylota=all_clustrs, cid=keep)
+#  smmry <- summary(selected)
+
+## ----selection3, eval=FALSE, message=FALSE, warning=FALSE, include=TRUE, paged.print=FALSE----
+#  cid <- smmry[2, 'ID']
+#  # get the cluster record
+#  cluster_record <- selected@cls[[cid]]
+#  # use the seq. IDs to get the sequence records
+#  seq_records <- selected@sqs[cluster_record@sids]
+#  # extract a single record
+#  seq_record <- seq_records[[seq_records@ids[[1]]]]
+#  # get the sequence
+#  seq <- rawToChar(seq_record@sq)
+
+## ----selection4, eval=FALSE, message=FALSE, warning=FALSE, include=TRUE, paged.print=FALSE----
+#  # choose best sequence per species
+#  reduced <- drop_by_rank(phylota=selected, rnk='species', n=1)
+#  # get txids at the species level for each sequence
+#  txids <- get_txids(phylota=reduced, cid=cid, rnk='species')
+#  # look up name for txids
+#  scientific_names <- get_tx_slot(phylota=reduced, txid=txids, slt_nm='scnm')
+#  # clean the names
+#  scientific_names <- gsub('\\.', '', scientific_names)
+#  scientific_names <- gsub('\\s+', '_', scientific_names)
+#  # look up sequence IDs for our chosen cluster
+#  sids <- reduced@cls[[cid]]@sids
+#  # write out
+#  write_sqs(phylota=reduced, sid=sids, outfile='cytb.fasta',
+#            sq_nm=scientific_names)
 
 ## ----testing, eval=FALSE, message=FALSE, warning=FALSE, include=TRUE, paged.print=FALSE----
 #  library(ape)
-#  system(paste0('mafft --auto ', clstr_id, '.fasta > alignment.fasta'))
+#  system('mafft --auto cytb.fasta > alignment.fasta')
 #  system(paste0('raxmlHPC -m GTRGAMMA -f a -N 10 -p 1234 -x 1234 -n aotus -s alignment.fasta'))
 #  tree <- read.tree(file='RAxML_bestTree.aotus')
-#  plot(tree, no.margin=TRUE) # check if the same tax IDs are grouped
+#  plot(tree, no.margin=TRUE, type='unrooted')
 
