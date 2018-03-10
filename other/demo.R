1efea4b79df8cf66a605d6ff14dbca8fbf2f9cb8
Dom Bennett <dominic.john.bennett@gmail.com>
2018-03-10 16:00:46 +0100

    Created demos

--- other/demo.R
+++ other/demo.R
@@ -1,14 +1,21 @@
 library(phylotaR)
-wd <- '/home/dom/Desktop/demo'
-ncbi_execs <- setUpNcbiTools(d='/home/dom/Programs/ncbi-blast-2.7.1+/bin')
-txid <- 0000
-setUpPrmtrs(wd=wd, txid=txid,
-            ncbi_execs=ncbi_execs)
-# Generate taxonomic 'nodes'
-runTaxise(wd)
-# Download sequences
-runDownload(wd)
-# Generate clusters
-runClusters(wd)
+tdpth <- ''   # Provide the path to the NCBI folder, if excluded program will download automatically
+wd <- ''      # Provide file path to working dir where output should be stored
+ncbi_dr <- '' # Provide filepath to NCBI blast tools, if tools are on syspath, leave as is.
+txid <- 9504  # NCBI txid of taxon choice
+setUp(wd=wd, txid=txid, ncbi_dr=ncbi_dr, tdpth=tdpth)
+run(wd=wd, nstages=3)
+# reset(wd=wd, stage='cluster')  # pipeline can be reset and restarted
+# restart(wd=wd, nstages=3)
 
+# Early development of clusters object tools
+clstrs_obj <- genClstrsObj(wd)
+clstr_id <- getBestClstrs(clstrs_obj, n=1)
+writeSeqs(clstrs_obj, clstr_id, wd) # writes sequences to [clstr_id] + .fasta
 
+# if you want, run quick mafft and raxml
+system(paste0('mafft --auto ', clstr_id, '.fasta > alignment.fasta'))
+system(paste0('raxmlHPC -m GTRGAMMA -f a -N 10 -p 1234 -x 1234 -n aotus -s alignment.fasta'))
+library(ape)
+tree <- read.tree(file='RAxML_bestTree.aotus')
+plot(tree, no.margin=TRUE) # check if the same taxa are grouped
--- other/dev.R
+++ other/dev.R
@@ -1,40 +1,45 @@
-# VARS
-id <- 'AJ428944'
-# AJ428944, AF434826
-mn_lngth <- 250
-mx_lngth <- 2500
 
-# RETRIEVE
-raw_rcrd <- rentrez::entrez_fetch(db="nucleotide",
-                                 rettype='gbwithparts',
-                                 retmode='text', id=id)
-rcrd <- XML::xmlToList(raw_rcrd)
+devtools::load_all('~/Coding/phylotaR')
 
-# KEY INFO
-id <- rcrd[['GBSeq']][['GBSeq_primary-accession']]
-ml_typ <- rcrd[['GBSeq']][['GBSeq_moltype']]
 
-# FEATURE
-fltr_tbl <- rcrd[['GBSeq']][['GBSeq_feature-table']]
-fltr <- fltr_tbl[[2]]
-ids <- nms <- strts <- stps <- NULL
-for(i in seq_along(fltr_tbl)) {
-  fltr <- fltr_tbl[[i]]
-  print(i)
-  if(fltr[['GBFeature_key']] == 'gene') {
-    lctn <- fltr[['GBFeature_location']]
-    if(!grepl('^[0-9\\.]+$', lctn)) {
-      next
-    }
-    strtstp <- strsplit(x=lctn, split='\\.\\.')[[1]]
-    strtstp <- as.numeric(strtstp)
-    sqlngth <- strtstp[2] - strtstp[1]
-    if(sqlngth > mn_lngth & sqlngth < mx_lngth) {
-      nm <- fltr[['GBFeature_quals']][['GBQualifier']][['GBQualifier_value']]
-      nms <- c(nms, nm)
-      strts <- c(strts, strtstp[1])
-      stps <- c(stps, strtstp[2])
-      ids <- c(ids, paste0(id, '/', lctn))
-    }
-  }
-}
+
+
+# INPUT
+ptable <- read_phylota(wd)
+
+# CLTYPE STATS
+# drop clusters of 10
+# n_taxa <- get_ntaxa(ptable, cid=ptable@cids)
+# n_taxa <- get_cl_slot(ptable, cid=ptable@cids, slt_nm='ntx')
+# keep <- names(n_taxa)[n_taxa > 10]
+keep <- ptable@cids[1:100]
+ptable <- drop_cls(ptable, keep)
+table(sapply(ptable@cls@cls, function(x) x@typ))
+
+# REDUCE
+# get n taxa per cluster
+n_taxa <- get_ntaxa(ptable, cid=ptable@cids)
+# drop all clusters with fewer than 100 taxa
+keep <- names(n_taxa)[n_taxa > 100]
+ptable <- drop_cls(ptable, keep)
+
+# FILTER
+fltrd <- drop_by_rank(ptable, rnk='genus', n=2,
+                      choose_by=c('nncltds'),
+                      greatest=c(TRUE))
+
+# SUMMARISE
+smmry <- summary_phylota(fltrd)
+smmry[grepl('rna', smmry$Feature),]
+smmry <- smmry[order(smmry$N_taxa, decreasing=TRUE), ]
+slctd_smmry <- smmry[smmry[['MAD']] > 0.6, ]
+slctd_smmry <- slctd_smmry[1:10, ]
+slctd_smmry$ID <- as.numeric(slctd_smmry$ID)
+
+# KEEP ONLY TOP TEN
+slctd <- drop_cls(fltrd, as.character(slctd_smmry$ID))
+
+# GET GENUS NAMES
+txids <- get_txids(slctd, cid='0', rnk='genus')
+genus_names <- get_tx_slot(slctd, txids, 'scnm')
+table(genus_names)
