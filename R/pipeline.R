31d289ddce05684900673eff8c66505072d121be
Dom Bennett <dominic.john.bennett@gmail.com>
2018-05-29 14:17:08 +0200

    reorganised and renamed

--- R/pipeline.R
+++ R/pipeline.R
@@ -1,5 +1,4 @@
-
-#' @name setUp
+#' @name setup
 #' @title Set-up parameters
 #' @description Set up working directory with parameters.
 #' @param wd Working directory
@@ -9,7 +8,7 @@
 #' @param ... Additional parameters
 #' @export
 #' @seealso \code{\link{parameters}}
-setUp <- function(wd, txid, ncbi_dr='.', v=FALSE,
+setup <- function(wd, txid, ncbi_dr='.', v=FALSE,
                   ...) {
   # header log
   msg <- paste0('phylotaR: Implementation of PhyLoTa in R [v',
@@ -47,86 +46,8 @@ setUp <- function(wd, txid, ncbi_dr='.', v=FALSE,
 #' \code{\link{runDownload}}, \code{\link{runClusters}},
 #' \code{\link{runClusters2}}
 run <- function(wd, nstages=4) {
-  stgs_msg <- chckStgs(frm=1, to=nstages)
-  runStgs(wd=wd, frm=1, to=nstages, stgs_msg=stgs_msg)
-}
-
-#' @name runStgs
-#' @title Sequentially run each stage
-#' @description Runs stages from \code{frm} to \code{to}.
-#' Records stage progress in cache.
-#' @param wd Working directory
-#' @param to Total number of stages to run
-#' @param frm Starting stage to run from
-#' @param stgs_msg Printout stage message for log
-#' @param rstrt Restarting, T/F
-#' @export
-runStgs <- function(wd, to, frm, stgs_msg, rstrt=FALSE) {
-  .run <- function() {
-    if(frm <= 1 & to >= 1) {
-      if(!ps[['v']]) {
-        cat('... Taxise\n')
-      }
-      runTaxise(wd)
-      svPrgrss(wd, 'taxise')
-    }
-    if(frm <= 2 & to >= 2) {
-      if(!ps[['v']]) {
-        cat('... Download\n')
-      }
-      runDownload(wd)
-      svPrgrss(wd, 'download')
-    }
-    if(frm <= 3 & to >= 3) {
-      if(!ps[['v']]) {
-        cat('... Cluster\n')
-      }
-      runClusters(wd)
-      svPrgrss(wd, 'cluster')
-    }
-    if(frm <= 4 & to >= 4) {
-      if(!ps[['v']]) {
-        cat('... Cluster2\n')
-      }
-      runClusters2(wd)
-      svPrgrss(wd, 'align')
-    }
-  }
-  ps <- ldPrmtrs(wd)
-  # header log
-  if(rstrt) {
-    msg <- paste0('Restarting pipeline on [', .Platform$OS.type,
-                  '] at [', Sys.time(), ']')
-  } else {
-    msg <- paste0('Running pipeline on [', .Platform$OS.type,
-                  '] at [', Sys.time(), ']')
-  }
-  brdr <- paste0(rep('-', nchar(msg)), collapse='')
-  msg <- paste0(brdr, '\n', msg, '\n', brdr)
-  info(ps=ps, lvl=1, msg)
-  info(ps=ps, lvl=1, stgs_msg)
-  errmsg <- try(.run(), silent=TRUE)
-  if('try-error' %in% is(errmsg)) {
-    # ctrl+c
-    if(grepl('Operation was aborted by an application callback',
-             errmsg[[1]])) {
-      msg <- paste0('---- Halted by user [', Sys.time(),
-                    '] ----')
-      .log(v=ps[['v']], wd=ps[['wd']], msg)
-      stop(msg)
-    }
-    # unexpected pipeline error
-    msg <- paste0('Unexpected ', errmsg[[1]], '\n',
-                  'Occurred [', Sys.time(), ']\n',
-                  'Contact package maintainer for help.\n')
-    .log(v=ps[['v']], wd=ps[['wd']], msg)
-    stop(msg)
-  }
-  # footer log
-  msg <- paste0('Completed pipeline at [', Sys.time(), ']')
-  brdr <- paste0(rep('-', nchar(msg)), collapse='')
-  msg <- paste0(brdr, '\n', msg, '\n', brdr)
-  info(ps=ps, lvl=1, msg)
+  stgs_msg <- stage_args_check(frm = 1, to = nstages)
+  stages_run(wd = wd, frm = 1, to = nstages, stgs_msg = stgs_msg)
 }
 
 #' @name restart
@@ -139,39 +60,18 @@ runStgs <- function(wd, to, frm, stgs_msg, rstrt=FALSE) {
 #' @seealso \code{\link{run}}
 restart <- function(wd, nstages=4) {
   stg <- rdPrgrss(wd)
-  if(is.na(stg)) {
+  if (is.na(stg)) {
     stop('Pipeline already complete. Use `reset()` to re-run pipeline.')
   }
   frm <- which(c('taxise', 'download', 'cluster', 'cluster2')
                 %in% stg)
-  if(frm > nstages) {
+  if (frm > nstages) {
     stop('Pipeline has already completed [', nstages,
          '] stages. Increase `nstages`.')
   }
-  stgs_msg <- chckStgs(frm=frm, to=nstages)
-  runStgs(wd=wd, frm=frm, to=nstages, stgs_msg=stgs_msg, rstrt=TRUE)
-}
-
-#' @name chckStgs
-#' @title Check to and frm stage numbers
-#' @description Ensure stage numbers, return stage print message.
-#' @param frm Starting stage
-#' @param to Ending stage
-#' @export
-#' @seealso \code{\link{run}}
-chckStgs <- function(frm, to) {
-  if(to < 1 | frm < 1) {
-    stop('Total stages to run cannot be less than 1.')
-  }
-  if(to > 4 | frm > 4) {
-    stop('Total stages to run cannot be more than 4.')
-  }
-  if(frm > to) {
-    stop('Starting stage must always come before ending stage.')
-  }
-  stgs <- c('taxise', 'download', 'cluster', 'cluster2')
-  stgs_msg <- paste0(stgs[frm:to], collapse = ', ')
-  paste0('Running stages: ', stgs_msg)
+  stgs_msg <- stage_args_check(frm = frm, to = nstages)
+  stages_run(wd = wd, frm = frm, to = nstages, stgs_msg = stgs_msg,
+             rstrt = TRUE)
 }
 
 #' @name reset
@@ -195,14 +95,14 @@ reset <- function(wd, stage, hard=FALSE) {
   info(ps=ps, lvl=1, msg)
 }
 
-#' @name setParameters
+#' @name parameters_reset
 #' @title Change parameters in a working directory
 #' @description Reset parameters afater running \code{setUp()}.
 #' @param wd Working directory
 #' @param parameters Parameters to be changed
 #' @param values New values for each parameter
 #' @export
-setParameters <- function(wd, parameters, values) {
+parameters_reset <- function(wd, parameters, values) {
   # TODO: make parameters an object with pre-defined parameter types
   ps <- ldPrmtrs(wd)
   for(i in 1:length(parameters)) {
