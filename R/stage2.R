f2ed96d23c18070d3a72f5ab56b729012cf01f7e
Dominic Bennett <dominic.john.bennett@gmail.com>
2018-09-15 19:12:44 +0200

    restez statements

--- R/stage2.R
+++ R/stage2.R
@@ -17,6 +17,11 @@ download_run <- function(wd) {
   clds_ids <- clade_select(txdct = txdct, ps = ps)
   info(lvl = 1, ps = ps, 'Identified [', length(clds_ids),
        '] suitable clades.')
+  if (!is.null(restez::restez_path_get())) {
+    info(lvl = 1, ps = ps, 'Connecting to restez database ...')
+    suppressMessages(restez::restez_connect())
+    on.exit(restez::restez_disconnect())
+  }
   info(lvl = 1, ps = ps, 'Downloading hierarchically ...')
   seq_download(txids = clds_ids, txdct = txdct, ps = ps)
   msg <- paste0('Completed stage DOWNLOAD: [', Sys.time(), ']')
