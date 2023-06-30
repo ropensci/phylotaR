dc9f93f8e57abb098f06606c21f0015b7c0a484b
Shixiang Wang (王诗翔) <wangshx@shanghaitech.edu.cn>
2023-06-30 02:09:52 +0000

    add document for data objects

---
+++ R/data.R
@@ -0,0 +1,87 @@
+#' aotus
+#' @docType data
+#' @name aotus
+#' @format A TreeMan or Phylota object
+#' @examples
+#' data("aotus")
+NULL
+
+#' birds
+#' @docType data
+#' @name birds
+#' @format A TreeMan or Phylota object
+#' @examples
+#' data("birds")
+NULL
+
+#' bromeliads
+#' @docType data
+#' @name bromeliads
+#' @format A TreeMan or Phylota object
+#' @examples
+#' data("bromeliads")
+NULL
+
+#' cycads
+#' @docType data
+#' @name cycads
+#' @format A TreeMan or Phylota object
+#' @examples
+#' data("cycads")
+NULL
+
+#' dragonflies
+#' @docType data
+#' @name dragonflies
+#' @format A TreeMan or Phylota object
+#' @examples
+#' data("dragonflies")
+NULL
+
+#' mammals
+#' @docType data
+#' @name mammals
+#' @format A TreeMan or Phylota object
+#' @examples
+#' data("mammals")
+NULL
+
+#' plants
+#' @docType data
+#' @name plants
+#' @format A TreeMan or Phylota object
+#' @examples
+#' data("plants")
+NULL
+
+#' sturgeons
+#' @docType data
+#' @name sturgeons
+#' @format A TreeMan or Phylota object
+#' @examples
+#' data("sturgeons")
+NULL
+
+#' tardigrades
+#' @docType data
+#' @name tardigrades
+#' @format A TreeMan or Phylota object
+#' @examples
+#' data("tardigrades")
+NULL
+
+#' tinamous
+#' @docType data
+#' @name tinamous
+#' @format A TreeMan or Phylota object
+#' @examples
+#' data("tinamous")
+NULL
+
+#' yeasts
+#' @docType data
+#' @name yeasts
+#' @format A TreeMan or Phylota object
+#' @examples
+#' data("yeasts")
+NULL
